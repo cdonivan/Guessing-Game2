8a69df136f11b24b65feb6a40d1ae520832f3891
cdonivan <cdonivan@gmail.com>
2015-11-14 21:06:29 -0800

    added 4th and 5th questions and updated HTML

--- game.js
+++ game.js
@@ -10,52 +10,92 @@ else if (Question1 == "no" || Question1 == "n" || Question1 == "NO" || Question1
 }
 else {
 	alert("Please phrase your answer as yes or no.");
-	var Question2=prompt("Is the sky blue?");
-	console.log(Question2);
-	if (Question2 == "yes" || Question2 == "y" || Question2 == "YES" || Question2 == "Y" || Question2 == "Yes") {
+	var Question1alt=prompt("Is the sky blue?");
+	console.log(Question1alt);
+	if (Question1alt == "yes" || Question1alt == "y" || Question1alt == "YES" || Question1alt == "Y" || Question1alt == "Yes") {
 	alert("That's right! (Should be, anyways.)");
 } 
-	else if (Question2 == "no" || Question2 == "n" || Question2 == "NO" || Question2 == "N" || Question2 == "No") {
+	else if (Question1alt == "no" || Question1alt == "n" || Question1alt == "NO" || Question1alt == "N" || Question1alt == "No") {
 	alert("Underneath all that grey is a bit of blue.");
 }
 }
 
-var Question3=prompt("Is Andorra in the Andes?");
-console.log(Question3);
-if (Question3 == "yes" || Question3 == "y" || Question3 == "YES" || Question3 == "Y" || Question3 == "Yes") {
+var Question2=prompt("Is Andorra in the Andes?");
+console.log(Question2);
+if (Question2 == "yes" || Question2 == "y" || Question2 == "YES" || Question2 == "Y" || Question2 == "Yes") {
 	alert("No, silly, the Andes are in South America and Andorra is in Europe!");
 } 
-else if (Question3 == "no" || Question3 == "n" || Question3 == "NO" || Question3 == "N" || Question3 == "No") {
+else if (Question2 == "no" || Question2 == "n" || Question2 == "NO" || Question2 == "N" || Question2 == "No") {
 	alert("Can't fool you. The Andes are in South America and Andorra is in Europe.");
 }
 else {
 	alert("Please phrase your answer as yes or no.");
-	var Question4=prompt("Is Andorra in the Andes?");
-	console.log(Question4);
-	if (Question4 == "yes" || Question4 == "y" || Question4 == "YES" || Question4 == "Y" || Question4 == "Yes") {
+	var Question2alt=prompt("Is Andorra in the Andes?");
+	console.log(Question2alt);
+	if (Question2alt == "yes" || Question2alt == "y" || Question2alt == "YES" || Question2alt == "Y" || Question2alt == "Yes") {
 	alert("No, silly, the Andes are in South America and Andorra is in Europe!");
 } 
-	else if (Question4 == "no" || Question4 == "n" || Question4 == "NO" || Question4 == "N" || Question4 == "No") {
+	else if (Question2alt == "no" || Question2alt == "n" || Question2alt == "NO" || Question2alt == "N" || Question2alt == "No") {
 	alert("Can't fool you. The Andes are in South America and Andorra is in Europe.");
 }
 }
 
-var Question5=prompt("Aren't Christmas lights the best?");
-console.log(Question5);
-if (Question5 == "yes" || Question5 == "y" || Question5 == "YES" || Question5 == "Y" || Question5 == "Yes") {
+var Question3=prompt("Aren't Christmas lights the best?");
+console.log(Question3);
+if (Question3 == "yes" || Question3 == "y" || Question3 == "YES" || Question3 == "Y" || Question3 == "Yes") {
 	alert("I couldn't agree more!");
 } 
-else if (Question5 == "no" || Question5 == "n" || Question5 == "NO" || Question5 == "N" || Question5 == "No") {
+else if (Question3 == "no" || Question3 == "n" || Question3 == "NO" || Question3 == "N" || Question3 == "No") {
 	alert("What could possibly be better than Christmas lights?");
 }
 else {
 	alert("Please phrase your answer as yes or no.");
-	var Question6=prompt("Aren't Christmas lights the best?");
-	console.log(Question6);
-	if (Question6 == "yes" || Question6 == "y" || Question6 == "YES" || Question6 == "Y" || Question6 == "Yes") {
+	var Question3alt=prompt("Aren't Christmas lights the best?");
+	console.log(Question3alt);
+	if (Question3alt == "yes" || Question3alt == "y" || Question3alt == "YES" || Question3alt == "Y" || Question3alt == "Yes") {
 	alert("I couldn't agree more!");
 } 
-	else if (Question6 == "no" || Question6 == "n" || Question6 == "NO" || Question6 == "N" || Question6 == "No") {
+	else if (Question3alt == "no" || Question3alt == "n" || Question3alt == "NO" || Question3alt == "N" || Question3alt == "No") {
 	alert("What could possibly be better than Christmas lights?");
+}
+}
+
+var Question4=prompt("Did the Toronto Blue Jays win the 2015 World Series?");
+console.log(Question4);
+if (Question4 == "yes" || Question4 == "y" || Question4 == "YES" || Question4 == "Y" || Question4 == "Yes") {
+	alert("Nah, it was the Kansas City Royals this year.");
+} 
+else if (Question4 == "no" || Question4 == "n" || Question4 == "NO" || Question4 == "N" || Question4 == "No") {
+	alert("Sad, isn't it? Next year, for sure!");
+}
+else {
+	alert("Please phrase your answer as yes or no.");
+	var Question4alt=prompt("Did the Toronto Blue Jays win the 2015 World Series?");
+	console.log(Question4alt);
+	if (Question4alt == "yes" || Question4alt == "y" || Question4alt == "YES" || Question4alt == "Y" || Question4alt == "Yes") {
+	alert("Nah, it was the Kansas City Royals this year.");
+} 
+	else if (Question4alt == "no" || Question4alt == "n" || Question4alt == "NO" || Question4alt == "N" || Question4alt == "No") {
+	alert("Sad, isn't it? Next year, for sure!");
+}
+}
+
+var Question5=prompt("Is it fair to say that Sandra Cisneros and Virginia Woolf share the same prized possession?");
+console.log(Question5);
+if (Question5 == "yes" || Question5 == "y" || Question5 == "YES" || Question5 == "Y" || Question5 == "Yes") {
+	alert("I agree. A room (or a house) of one's own.");
+} 
+else if (Question5 == "no" || Question5 == "n" || Question5 == "NO" || Question5 == "N" || Question5 == "No") {
+	alert("Eh, I'd argue that the difference between a room and a house is not the point.");
+}
+else {
+	alert("Please phrase your answer as yes or no.");
+	var Question5alt=prompt("Is it fair to say that Sandra Cisneros and Virginia Woolf share the same prized possession?");
+	console.log(Question5alt);
+	if (Question5alt == "yes" || Question5alt == "y" || Question5alt == "YES" || Question5alt == "Y" || Question5alt == "Yes") {
+	alert("I agree. A room (or a house) of one's own.");
+} 
+	else if (Question5alt == "no" || Question5alt == "n" || Question5alt == "NO" || Question5alt == "N" || Question5alt == "No") {
+	alert("Eh, I'd argue that the difference between a room and a house is not the point.");
 }
 }
