1768da3edd2ddf3234b71ac78f4c0806733d47c8
cdonivan <cdonivan@gmail.com>
2015-11-14 20:31:04 -0800

    adding README file

---
+++ README.md
@@ -0,0 +1,4 @@
+Christina Donivan
+Project: Guessing Game
+Description: A guessing game to demonstrate learning throughout Foundations 1.
+Sources: TBD
--- game.js
+++ game.js
@@ -15,7 +15,7 @@ else {
 	if (Question2 == "yes" || Question2 == "y" || Question2 == "YES" || Question2 == "Y" || Question2 == "Yes") {
 	alert("That's right! (Should be, anyways.)");
 } 
-else if (Question2 == "no" || Question2 == "n" || Question2 == "NO" || Question2 == "N" || Question2 == "No") {
+	else if (Question2 == "no" || Question2 == "n" || Question2 == "NO" || Question2 == "N" || Question2 == "No") {
 	alert("Underneath all that grey is a bit of blue.");
 }
 }
@@ -35,7 +35,7 @@ else {
 	if (Question4 == "yes" || Question4 == "y" || Question4 == "YES" || Question4 == "Y" || Question4 == "Yes") {
 	alert("No, silly, the Andes are in South America and Andorra is in Europe!");
 } 
-else if (Question4 == "no" || Question4 == "n" || Question4 == "NO" || Question4 == "N" || Question4 == "No") {
+	else if (Question4 == "no" || Question4 == "n" || Question4 == "NO" || Question4 == "N" || Question4 == "No") {
 	alert("Can't fool you. The Andes are in South America and Andorra is in Europe.");
 }
 }
@@ -55,7 +55,7 @@ else {
 	if (Question6 == "yes" || Question6 == "y" || Question6 == "YES" || Question6 == "Y" || Question6 == "Yes") {
 	alert("I couldn't agree more!");
 } 
-else if (Question6 == "no" || Question6 == "n" || Question6 == "NO" || Question6 == "N" || Question6 == "No") {
+	else if (Question6 == "no" || Question6 == "n" || Question6 == "NO" || Question6 == "N" || Question6 == "No") {
 	alert("What could possibly be better than Christmas lights?");
 }
 }
